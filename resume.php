5988f156f050afe04a045cefe98ec81272860ba3
mercadoCODING <raphaelandremercado@gmail.com>
2024-05-28 14:29:59 +0800

    2nd commit

--- resume.php
+++ resume.php
@@ -3,9 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Mercado Raphael Andre">
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/instagram.css' rel='stylesheet'>
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/facebook.css' rel='stylesheet'>
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/mail.css' rel='stylesheet'>
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/phone.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
     <title>Resume</title>
 </head>
 <body>
+
+    <header>
+        <h1>Resume Page</h1>
+    </header>
+
+    <section class="main-container">
+        <div class ="image-container">
+         <img src="image\image.jpg" alt="mercado-img">
+        </div>
+
+        <div class ="personal-information">
+            <ul>
+                <li><h1>Raphael Andre Mercado</h1></li>
+                <li>Pasig City</li>
+                <li><i class="gg-mail"></i><br\>202211187@fit.edu.ph</li>
+                <li><i class="gg-phone"></i><br\>63+ 9394261614</li>
+            </ul>
+            <div class = "social-bars">
+                <h3>Social Media</h3>
+                <div class="icon-wrapper">
+                    <a href="https://www.instagram.com/mercadora_ph/"><i class="gg-instagram"></i></a>
+                </div>
+                <div class="icon-wrapper">
+                    <a href="https://www.facebook.com/meguminarchwi"><i class="gg-facebook"></i></a>
+                </div>
+            </div>
+                
+        </div>
+        <div class ="info-container">
+
+    
+        </div>
+    </section>  
     
 </body>
 </html>
